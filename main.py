f504626481bf3504b844aff99457f7f76bb474c5
beka0708 <abdulasanovbektur@gmail.com>
2026-06-05 17:13:16 +0600

    lesson 3

--- main.py
+++ main.py
@@ -3,22 +3,32 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Мое первое приложение!'
-    page.theme_mode = ft.ThemeMode.LIGHT
+    page.theme_mode = ft.ThemeMode.DARK
 
-    text_hello = ft.Text("Привет, Группа 66-1!", size=24)
+    def text_name(e):   # e - event
+        name = text_input.value.strip()
 
-    text_button = ft.TextButton('SEND')
-    elavated_button = ft.ElevatedButton('SEND')
-    icon_button = ft.IconButton(icon=ft.Icons.SEND)
+        if name:
+            text_hello.value = f"Добро пожаловать! {text_input.value}"
+            text_hello.color = ft.Colors.GREEN_900
+            text_input.value = ""
+        else:
+            text_hello.value = f"Поле ввода имени не заполнено!"
+            text_hello.color = ft.Colors.RED_900
+            
+    text_hello = ft.Text('Hello', size=25)
+    text_input = ft.TextField(label="Введите свое имя", on_submit=text_name)
+    btn = ft.ElevatedButton('SEND', on_click=text_name)
 
-    name_input = ft.TextField(label="Введите имя")
+    def thememode(e):
+        if page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        else:
+            page.theme_mode = ft.ThemeMode.DARK
+        
+    
+    theme_btn = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7, on_click=thememode)
 
-    page.add(
-        text_hello,
-        name_input,
-        text_button, 
-        elavated_button, 
-        icon_button, 
-    )
+    page.add(text_hello, text_input, btn, theme_btn)
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
